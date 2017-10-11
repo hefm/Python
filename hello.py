3f3382062ac6c72e107fe5b2095f9c9fe9055e92
hfm <392699146@qq.com>
2017-10-11 10:48:45 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+#Linux/OS X系统，这是一个Python可执行程序，Windows系统会忽略这个注释
+#!/usr/bin/env python3
+#Python解释器，按照UTF-8编码读取源代码
+# -*- coding: utf-8 -*-
+
+L = [ ['Apple', 'Google', 'Microsoft'], ['Java', 'Python', 'Ruby', 'PHP'], ['Adam', 'Bart', 'Lisa'] ]
+print(L[0][0])
+print(L[1][1])
+print(L[2][2])
